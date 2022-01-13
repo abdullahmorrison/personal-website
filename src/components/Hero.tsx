a5395382d62c447bb8bf0280e724be637f51751d
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-01-13 16:45:29 -0500

    Hero update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,13 @@
+import styles from './hero.module.css'
+
 const Hero=()=>{
     return(
-        <header>
+        <header className={styles.header}>
             <h1>Hi, My Name is Abdullah Morrison</h1>
             <h2>I'm a Full-Stack Developer</h2>
-            <section className="call-to-action">
-                <button>Contact Me</button>
-                <button>View My Resume</button>
+            <section className={styles.callToAction}>
+                <button><a href="mailto:abdullahmorrison@gmail.com">Contact Me</a></button>
+                <button><a href={process.env.PUBLIC_URL + 'Resume.pdf'} download>View My Resume</a></button>
             </section>
         </header>
     )
