fa4a84fb57d68616a6e08524975f5a23e76e5f76
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-01-13 15:25:17 -0500

    created hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero=()=>{
+    return(
+        <header>
+            <h1>Hello, My Name is Abdullah Morrison</h1>
+            <h2>I'm a Full-Stack Developer</h2>
+            <section className="call-to-action">
+                <button>Contact Me</button>
+                <button>View My Resume</button>
+            </section>
+        </header>
+    )
+}
+export default Hero
