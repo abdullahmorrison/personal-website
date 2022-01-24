39e7a274f7d0c25627f0e0d6344e9e48f03e09e1
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-01-24 11:04:21 -0500

    improved html button semantics

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,8 +6,8 @@ const Hero=()=>{
             <h1 className={styles.selfIntro}>Hi, My Name is Abdullah Morrison</h1>
             <h2 className={styles.selfIntro}>I'm a Full-Stack Developer</h2>
             <section className={styles.callToAction}>
-                <button><a href="mailto:abdullahmorrison@gmail.com">Contact Me</a></button>
-                <button><a href={process.env.PUBLIC_URL + 'Resume.pdf'} download>View My Resume</a></button>
+                <a href="mailto:abdullahmorrison@gmail.com" className={styles.button}>Contact Me</a>
+                <a href={process.env.PUBLIC_URL + 'Resume.pdf'} download className={styles.button}>View My Resume</a>
             </section>
         </header>
     )
