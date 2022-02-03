5584c573970c79e9974e16463b1c803f76ace472
Abdullah Morrison <amorr128@uottawa.ca>
2022-02-03 12:46:23 -0500

    changed hero title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero=()=>{
     return(
         <header className={styles.header}>
             <h1 className={styles.selfIntro}>Hi, My Name is Abdullah Morrison</h1>
-            <h2 className={styles.selfIntro}>I'm a Full-Stack Developer</h2>
+            <h2 className={styles.selfIntro}>I am a Software Engineer</h2>
             <section className={styles.callToAction}>
                 <a href="mailto:abdullahmorrison@gmail.com" className={styles.button}>Contact Me</a>
                 <a href={process.env.PUBLIC_URL + 'Resume.pdf'} download className={styles.button}>View My Resume</a>
