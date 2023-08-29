afeacce9b7ecc2ea0534c761eb4f495e01ed4012
abdullahmorrison <abdullahmorrison@gmail.com>
2023-08-28 20:03:53 -0400

    added project section template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Nav from './nav'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
       <main className={styles.hero}>
         <Nav/>
         <h1 className={styles.name}>
@@ -11,6 +11,20 @@ export default function Home() {
           <span className={styles.lastName}><p>Morrison</p></span> 
         </h1>
       </main>
+
+      <section className={styles.project}>
+        <div className={styles.projectInfo}>
+          <h3 className={styles.number}>01</h3>
+          <h3 className={styles.name}>Motivation Scale</h3>
+          <p className={styles.description}>A mindfulness tool about motivation.</p>
+
+          <ul className={styles.links}>
+            <li><a href="">view project</a></li>
+            <li><a href="">git repo</a></li>
+          </ul>
+        </div>
+      </section>
+
     </div>
   )
 }
